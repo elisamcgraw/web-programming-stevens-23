b79097bf10b58f75d2810732f9b58de48c88d3cc
Elisa McGraw <elisamasiero.em@gmail.com>
2023-09-24 21:11:05 -0400

    Second Function safe check

--- lab2_stub/app.js
+++ lab2_stub/app.js
@@ -3,17 +3,23 @@ do not forget that you need to create the package.json and add the start command
 
 import {
     mergeCommonElements,
-    //findTriangle,
+    findTriangles,
     //stringMetrics
 
 } from './arrayUtils.js';
 
 try {
     /* Merge Common Elements */
-    console.log(mergeCommonElements(["apple", "banana", "cherry"], ["cherry", "grape", "kiwi"], [1, 2, 3])); // []
-    console.log(mergeCommonElements([1, [2, 3]], [[3, 4], [1, 2], [5, 6]], [3, [2, 3], 7])); // [2, 3]
+    //console.log(mergeCommonElements(["apple", "banana", "cherry"], ["cherry", "grape", "kiwi"], [1, 2, 3])); // []
+    //console.log(mergeCommonElements([1, [2, 3]], [[3, 4], [1, 2], [5, 6]], [3, [2, 3], 7])); // [2, 3]
+    console.log(findTriangles([[3,3,3], [3,3,4], [5,4,2]]));   // returns {'0': [3.9,9, "equilateral"], '1': [4.47,10, "isosceles"], '2': [3.8,11, "scalene"]}
+    console.log(findTriangles([[7,5,5], [2,4,3], [12,12,11]]));   // returns {'0': [12.5, 17, "isosceles"], '1': [2.90, 9, "scalene"], '2': [58.66,35, "isosceles"]}
+    console.log(findTriangles([])); // Should return an empty object since there are no triangles
+    console.log(findTriangles([[3, 4, 5], [1, 2, 3]]));
+    console.log(findTriangles([5, 5, 5])); // throws an error
 
-    
+
+
 } catch (e) {
   console.error(e); // Print the error message
 }
--- lab2_stub/arrayUtils.js
+++ lab2_stub/arrayUtils.js
@@ -3,13 +3,14 @@
       DO NOT CHANGE THE FUNCTION NAMES
 */
 
-import { flattenArray, customSort } from './helpers.js';
-  //this function takes in a variable number of arrays that's what the ...args signifies
+import { flattenArray, customSort } from "./helpers.js";
+
+/****************************** Error Checks *******************************/
 
 // Function to check if at least two arrays are supplied
 const checkInputArrayCount = (args) => {
   if (args.length < 2) {
-    throw new Error('At least two arrays must be supplied as input');
+    throw new Error("At least two arrays must be supplied as input");
   }
 };
 
@@ -22,8 +23,6 @@ const checkInputArrays = (args) => {
   });
 };
 
-
-
 // Function to check if each input array is not empty
 const checkNonEmptyArrays = (args) => {
   args.forEach((arr, index) => {
@@ -33,8 +32,60 @@ const checkNonEmptyArrays = (args) => {
   });
 };
 
+// Function to check if an argument is a 2D array of arrays
+const isArrayofArrays = (arr) => {
+  if (!Array.isArray(arr) || !arr.every((subArr) => Array.isArray(subArr))) {
+    throw new Error("Input must be a 2D array of arrays");
+  }
+};
 
-// Function to merge common elements from multiple arrays
+// Function to check if each subarray is a valid triangle
+const areSubarraysTriangles = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const sides = arr[i];
+
+    if (
+      sides.length !== 3 ||
+      !sides.every((side) => typeof side === "number")
+    ) {
+      throw new Error(`Input at index ${i} is not a valid triangle`);
+    }
+
+    const [a, b, c] = sides;
+
+    if (!(a + b > c && a + c > b && b + c > a)) {
+      throw new Error(`Input at index ${i} does not form a valid triangle`);
+    }
+  }
+};
+
+// Function to check if each subarray contains only numbers, is not empty, and trims empty spaces
+const areSubarraysValid = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let sides = arr[i].map((side) =>
+      typeof side === "string" ? side.trim() : side
+    ); // Trim empty spaces from sides if they are strings
+
+    if (
+      sides.length !== 3 ||
+      !sides.every((side) => typeof side === "number")
+    ) {
+      throw new Error(`Input at index ${i} is not a valid triangle`);
+    }
+
+    if (sides.some((side) => isNaN(side))) {
+      throw new Error(`Input at index ${i} contains non-numeric values`);
+    }
+
+    if (sides.some((side) => side <= 0)) {
+      throw new Error(`Input at index ${i} contains non-positive values`);
+    }
+  }
+};
+
+/****************************** Lab Functions Implementation  ******************************/
+
+//This function takes in a variable number of arrays that's what the ...args signifies
 let mergeCommonElements = (...args) => {
   // Check input array count
   checkInputArrayCount(args);
@@ -62,13 +113,41 @@ let mergeCommonElements = (...args) => {
   return uniqueSortedCommonElements;
 };
 
-// Export the mergeCommonElements function
-export { mergeCommonElements };
+let findTriangles = (arr) => {
+  // Check if the input is a 2D array of arrays
+  isArrayofArrays(arr);
 
+  // Check if each subarray is a valid triangle
+  areSubarraysTriangles(arr);
 
+  // Check if each subarray contains only numbers and is not empty
+  areSubarraysValid(arr);
 
+  const result = {};
 
+  for (let i = 0; i < arr.length; i++) {
+    const sides = arr[i];
+    const [a, b, c] = sides;
 
-//let findTriangles = (arr) => {};
+    const s = (a + b + c) / 2;
+    const area = parseFloat(
+      Math.sqrt(s * (s - a) * (s - b) * (s - c)).toFixed(2)
+    );
+    const perimeter = a + b + c;
 
-//let stringMetrics = (arr) => {};
+    let triangleType;
+    if (a === b && b === c) {
+      triangleType = "equilateral";
+    } else if (a === b || b === c || a === c) {
+      triangleType = "isosceles";
+    } else {
+      triangleType = "scalene";
+    }
+
+    result[i] = [area, perimeter, triangleType];
+  }
+
+  return result;
+};
+
+export { mergeCommonElements, findTriangles };
